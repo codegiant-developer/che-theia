e7d69e6bc28c5c406449e4cca9c3064fdd157db5
Jonathan Czitkovics <jonathan@codegiant.io>
2022-02-14 17:58:29 +0800

    fix for lag in typing for filtering plugins

--- extensions/eclipse-che-theia-plugin-ext/src/browser/plugin/che-plugin-view.tsx
+++ extensions/eclipse-che-theia-plugin-ext/src/browser/plugin/che-plugin-view.tsx
@@ -151,6 +151,7 @@ export class ChePluginView extends PluginWidget {
      */
     protected onFilterChanged(filter: string): void {
         this.filterString = filter;
+        this.update();
 
         if (this.status === 'filtering') {
             this.filter();
